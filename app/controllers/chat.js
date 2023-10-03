d1c5701f1e44d8c6dd7900146399292190091f96
ivo.souza <ivo.souza@varixx.com.br>
2023-10-03 13:57:19 -0300

    att

--- app/controllers/chat.js
+++ app/controllers/chat.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const jwt = require('jsonwebtoken');
 // const models = require(path.resolve('./app/models/chat.js'));
 
 
@@ -9,15 +10,44 @@ async function chatController(req, res){
 
 function socketController(io){
   console.log('aop')
-  io.on('connection', (socket)=> {
+  let tempChat = [];
+  let users = [];
+  let userList = [];
+  io.on('connect', (socket)=> {
 
-    socket.on("teste", (e) => {
-      console.log(e);
-    })
     console.log('usuario conectado');
+    socket.on('init', async (info) => {
+      const secret = process.env.JWT_SECRET
+      const token = info;
+    
+      jwt.verify(token, secret,(err, decoded) => {
+        if (err) {
+            console.error('Erro ao verificar o token:', err);
+        } else {
+            console.log('Token verificado com sucesso. Decodificado:', decoded);
+            tempUser = {sid: socket.id, nick: decoded.nick };
+            users.push(tempUser)
+            userList = getUserList(users);
+            socket.emit('attUser', userList);
+        }
+      });
+    })
+    socket.on('disconnect', () => {
+      users = users.filter((info)=>{
+        if (info.sid == socket.id){
+          console.log('desconectado%#%#%#%#', info)
+        }
+        return info.sid !== socket.id
+      })
+      userList = getUserList(users);
+      socket.emit('attUser', userList)
+    })
   })
+}
 
-
+function getUserList(arr) {
+  arr = arr.map(users => users.nick)
+  return arr;
 }
 
 module.exports =  { chatController, socketController };
--- readme.md
+++ readme.md
@@ -8,11 +8,11 @@
   - *Rotas*
   - *Models* -> modelos de banco ou consultas SQLs
   - *Views* -> paginas a serem redenrizadas
-  - *Controlers* -> funcoes com regras de negocio da aplicacao. ex: login, buscar usaurio, socketio e etc.
+  - *Controlers* -> funcoes com regras de negocio da aplicacao. ex: login, buscar usuario, socketio e etc.
 
 ### Dica
 
 - Estuda modelo *MVC* - model, view e controller.
 - Estuda *ORMs*.
-- Estuda sobre modelos de autentificacao como *JWT* e criptografica *bcript*.
+- Estuda sobre modelos de autentificacao como *JWT* e criptografia *bcript*.
 - Estudar sobre protocolos de comunicao web (HTTP, HTTPS)
